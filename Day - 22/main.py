1978ecc7fc6776868d0d4dbedc8cfee94d86b41b
Annapurna Gupta <ganu48600@gmail.com>
2024-09-11 01:39:35 +0530

    Update main.py

--- Day - 22/main.py
+++ Day - 22/main.py
@@ -1,12 +1,14 @@
 from turtle import Screen
 from create_paddle import Paddle
 from ball import Ball
-
+from scoreboard import Scoreboard
+import time
 
 screen = Screen()
-screen.tracer(1)
-r_p = Paddle((360, 0))
-l_p = Paddle((-360, 0))
+score = Scoreboard()
+screen.tracer(0)
+r_p = Paddle((350, 0))
+l_p = Paddle((-350, 0))
 screen.title("Pong Game")
 screen.setup(800, 600)
 screen.bgcolor("black")
@@ -20,10 +22,22 @@ screen.onkey(l_p.go_down, "s")
 
 is_game_on = True
 while is_game_on:
-    ball.speed(-1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
     if ball.ycor() > 278 or ball.ycor() < -278:
-        ball.bounce()
+
+        ball.bounce_y()
+
+    if ball.distance(r_p) < 50 and ball.xcor() > 330 or ball.distance(l_p) < 50 and ball.xcor() > -330:
+        ball.bounce_x()
+
+    if ball.xcor() > 400:
+        score.l_point()
+        ball.reset_position()
+
+    if ball.xcor() < -400:
+        score.r_point()
+        ball.reset_position()
 
 screen.exitonclick()
