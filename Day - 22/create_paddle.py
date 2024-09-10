b958f7370d626690736ec211e73398a3280dec1c
Annapurna Gupta <ganu48600@gmail.com>
2024-09-10 22:33:48 +0530

    Create create_paddle.py

---
+++ Day - 22/create_paddle.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(5, 1)
+        self.penup()
+        self.goto(position)
+
+    def position(self, x, y):
+        self.goto(x, y)
+
+    def go_up(self):
+        new_y = self.ycor() + 45
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 45
+        self.goto(self.xcor(), new_y)
